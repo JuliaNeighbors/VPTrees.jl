6f21c87017f5eeaba35f908d11a59f9b94ace50c
Alan Schelten <alan.schelten@mercateo.com>
2020-07-15 13:32:06 +0200

    Bad repo url

--- docs/make.jl
+++ docs/make.jl
@@ -6,12 +6,12 @@ makedocs(;
     pages=[
         "Home" => "index.md",
     ],
-    repo="https://github.com/altre/VPTrees.jl/blob/{commit}{path}#L{line}",
+    repo="https://github.com/JuliaNeighbors/VPTrees.jl/blob/{commit}{path}#L{line}",
     sitename="VPTrees.jl",
     authors="Alan Schelten",
     assets=String[],
 )
 
 deploydocs(;
-    repo="github.com/altre/VPTrees.jl",
+    repo="github.com/JuliaNeighbors/VPTrees.jl",
 )
